3a833ffae1d9fcc041c212eb9da4dd70a5256e7d
Liza Shulika <liza.shulika2009@gmail.com>
2025-01-14 21:43:06 +0200

    password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,16 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param("Pass@word1", True, id="one test"),
+        pytest.param("Str@ng", False, id="two test"),
+        pytest.param("qwerty1", False, id="three test"),
+
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    res = check_password(password)
+    assert res == result
